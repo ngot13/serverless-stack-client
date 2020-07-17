f80a2f32eda1af5eed4b2f41747bd0a475c2214e
Ngo <tonyngo@3c22fb772fa9.ant.amazon.com>
2020-07-17 15:18:22 -0700

    setting up our react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notes-app-upload-tn"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://kqlp042874.execute-api.us-east-1.amazonaws.com/prod\n"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_0jNUugv3O",
+        APP_CLIENT_ID: "24g3fk9ae2067khkoqb2jle009",
+        IDENTITY_POOL_ID: "us-east-1:980d6529-990a-4e6d-9e14-8ceed14583d1"
+    }
+};
